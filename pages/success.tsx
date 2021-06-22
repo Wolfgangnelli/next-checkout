449da109da59f9572dbe688410318f751d21c271
[AndreaS] <solfa_90@hotmail.it>
2021-06-22 18:07:18 +0200

    add confirm order success

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, {useEffect} from 'react'
 import Layout from '../layout/layout'
+import constants from '../constants';
+
+declare var Stripe;
 
 const Success = () => {
+    const router = useRouter();
+    const {source} = router.query;
+
+    useEffect(() => {
+      if(source !== undefined) {
+        (
+            async () => {
+              const {data} = await axios.post(`${constants.endpoint}/orders/confirm`, {
+                    source
+                })
+                console.log(data);
+            }
+        )();
+      }
+    }, [source])
     return (
         <Layout>
             <div className="min-h-screen text-center">
