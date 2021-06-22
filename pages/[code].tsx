aa75b13be506c8b9f8bc7a8c3d9e2ec00357c20d
[AndreaS] <solfa_90@hotmail.it>
2021-06-22 16:45:07 +0200

    submitting form data

--- pages/[code].tsx
+++ pages/[code].tsx
@@ -11,6 +11,25 @@ export default function Home() {
   const [user, setUser] = useState(null);
   const [products, setProducts] = useState([]);
   const [quantities, setQuantities] = useState([]);
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [zip, setZip] = useState("");
+
+
+  useEffect(() => {
+    setFirst_name("");
+    setLast_name("");
+    setEmail("");
+    setAddress("");
+    setCountry("");
+    setCity("");
+    setZip("");
+    setQuantities([]);
+  }, [])
 
 
   useEffect(() => {
@@ -51,6 +70,24 @@ export default function Home() {
       return s + q.quantity * product.price;
        
     }, 0)
+  }
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+  
+     const {data} = await axios.post(`${constants.endpoint}/orders`, {
+        code,
+        first_name,
+        last_name,
+        email,
+        address,
+        city,
+        country,
+        zip,
+        products: quantities
+      })
+      console.log(data);
+    
   }
 
 
@@ -95,34 +132,34 @@ export default function Home() {
           <div className="md:row-start-1 md:row-end-2 md:col-start-1 md:col-end-2 bg-white">
               <p className="text-gray-800 font-medium">Customer information</p>
             <div>
-            <form className="max-w-xl m-4 p-8 bg-white rounded shadow-xl">
+            <form className="max-w-xl m-4 p-8 bg-white rounded shadow-xl" onSubmit={submit}>
               <div className="">
                 <label className="block text-sm text-gray-00" htmlFor="first_name">First name</label>
-                <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="first_name" name="first_name" type="text" placeholder="Your Name" aria-label="FirstName" required />
+                <input onChange={(e) => setFirst_name(e.target.value)} className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="first_name" name="first_name" type="text" placeholder="Your Name" aria-label="FirstName" required />
               </div>
               <div className="mt-2">
                 <label className="block text-sm text-gray-00" htmlFor="last_name">Last name</label>
-                <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="last_name" name="last_name" type="text" placeholder="Your Surname" aria-label="LastName" required />
+                <input onChange={(e) => setLast_name(e.target.value)} className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="last_name" name="last_name" type="text" placeholder="Your Surname" aria-label="LastName" required />
               </div>
               <div className="mt-2">
                 <label className="block text-sm text-gray-600" htmlFor="email">Email</label>
-                <input className="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="email" name="email" type="text" placeholder="you@example.com" aria-label="Email" required />
+                <input onChange={(e) => setEmail(e.target.value)} className="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="email" name="email" type="text" placeholder="you@example.com" aria-label="Email" required />
               </div>
               <div className="mt-2">
                 <label className=" block text-sm text-gray-600" htmlFor="address">Address</label>
-                <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="address" name="address" type="text" placeholder="Street" aria-label="Address" required />
+                <input onChange={(e) => setAddress(e.target.value)} className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="address" name="address" type="text" placeholder="Street" aria-label="Address" required />
               </div>
               <div className="mt-2">
                 <label className="text-sm block text-gray-600" htmlFor="city">City</label>
-                <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="city" name="city" type="text" placeholder="City" aria-label="City" required />
+                <input onChange={(e) => setCity(e.target.value)} className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="city" name="city" type="text" placeholder="City" aria-label="City" required />
               </div>
               <div className="inline-block mt-2 w-1/2 pr-1">
                 <label className="block text-sm text-gray-600" htmlFor="country">Country</label>
-                <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="country" name="country" type="text" placeholder="Country" aria-label="Country" required />
+                <input onChange={(e) => setCountry(e.target.value)} className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="country" name="country" type="text" placeholder="Country" aria-label="Country" required />
               </div>
               <div className="inline-block mt-2 -mx-1 pl-1 w-1/2">
                 <label className="block text-sm text-gray-600" htmlFor="zip">Zip</label>
-                <input className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="zip"  name="zip" type="text"  placeholder="Zip" aria-label="Zip" required />
+                <input onChange={(e) => setZip(e.target.value)} className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="zip"  name="zip" type="text"  placeholder="Zip" aria-label="Zip" required />
               </div>
 {/*               <p className="mt-4 text-gray-800 font-medium">Payment information</p>
               <div className="">
