e115f9a29d72f4d597f7ca7efcf45fc598ef228f
[AndreaS] <solfa_90@hotmail.it>
2021-06-22 18:09:49 +0200

    fix bugs

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 export default {
     endpoint: "http://localhost:8000/api/checkout",
-    stripe_key: "pk_test_51J0oF5LPc1tJq14z8CESrZv3oadYiGy2EnEQAnIQKSMV8Yq2IS3PV7Oe7p37caJXWAebRU6zpVYA8LKh6ccpw8ya00gMKkNnSw"
+    stripe_key: "public_key"
 }
